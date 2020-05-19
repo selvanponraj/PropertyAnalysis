e10c0bee3663cf0a46c50b76e103aa9e4697f0e1
tturin <timturin@gmail.com>
2020-05-18 20:50:34 -0400

    Adding argument functionality to get_properties.js and creating get_properties function to separate from main

--- main/get_properties.js
+++ main/get_properties.js
@@ -1,13 +1,44 @@
 const realtor = require('realtorca');
 const fs = require('fs');
 
+// Set default values
+var minLong = -79.6758985519409;
+var maxLong = -79.6079635620117;
+var minLat = 43.57601549736786;
+var maxLat = 43.602250137362276;
+var minPrice = 100000;
+var maxPrice = 410000;
+
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+
+// Override default values with arguments if they exit
+if (typeof argv['minLong'] != 'undefined') {
+    minLong = Number(argv['minLong']);
+}
+if (typeof argv['maxLong'] != 'undefined') {
+    maxLong = Number(argv['maxLong']);
+}
+if (typeof argv['minLat'] != 'undefined') {
+    minLat = Number(argv['minLat']);
+}
+if (typeof argv['maxLat'] != 'undefined') {
+    maxLat = Number(argv['maxLat']);
+}
+if (typeof argv['minPrice'] != 'undefined') {
+    minPrice = Number(argv['minPrice']);
+}
+if (typeof argv['maxPrice'] != 'undefined') {
+    maxPrice = Number(argv['maxPrice']);
+}
+
 let opts = {
-	LongitudeMin: -79.6758985519409,
-	LongitudeMax: -79.6079635620117,
-	LatitudeMin: 43.57601549736786,
-	LatitudeMax: 43.602250137362276,
-	PriceMin: 100000,
-	PriceMax: 410000
+	LongitudeMin: minLong,
+	LongitudeMax: maxLong,
+	LatitudeMin: minLat,
+	LatitudeMax: maxLat,
+	PriceMin: minPrice,
+	PriceMax: maxPrice
 };
 
 realtor.post(opts)
